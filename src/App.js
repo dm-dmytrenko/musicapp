ad035653fe21f0a60c3929380a496a01b3ed6138
clounemusic <cloune.music@gmail.com>
2026-06-11 20:34:31 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Selector from './pages/Selector/Selector';
 
 function App() {
   return (
     <div className="App">
-      <Selector />
+      <Routes>
+        <Route path="/selector" element={<Selector />} />
+      </Routes>
     </div>
   );
 }
