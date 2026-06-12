44c6bb614226f6053b6b1fa8d9f47dd6a62db040
clounemusic <cloune.music@gmail.com>
2026-06-12 15:58:54 +0200

    add preview page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Selector from './pages/Selector/Selector';
 import UploadTrack from './pages/UploadTrack/UploadTrack';
+import Preview from './pages/Preview/Preview';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/upload" element={<UploadTrack />} />
         <Route path="/selector" element={<Selector />} />
+        <Route path="/preview" element={<Preview />} />
       </Routes>
     </div>
   );
